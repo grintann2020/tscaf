9c93c03aa9a87ecfdb6066806454daad88a5bb40
grintann <grintann2020@gmail.com>
2021-03-10 00:34:50 +0800

    init

---
+++ Assets/Scripts/Singleton.cs
@@ -0,0 +1,13 @@
+﻿namespace T {
+    public class Singleton<T> where T : new() {
+        protected static T _ins;
+        public static T Ins {
+            get {
+                if (_ins == null) {
+                    _ins = new T();
+                }
+                return _ins; // Returns the instance of this singleton.
+            }
+        }
+    }
+}
