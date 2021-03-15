6519a94bdce3e83653acfa6d2787fb6911e7503f
grintann <grintann2020@gmail.com>
2021-03-16 00:55:03 +0800

    ScrRec v1

--- Assets/Scripts/GameMgr.cs
+++ Assets/Scripts/GameMgr.cs
@@ -5,38 +5,30 @@ namespace T {
     public class GameMgr : Singleton<GameMgr> {
 
         private Settings _settings;
-        // private ScrRezSO[] _scrRezArr;
-        // private ScrRezSO _currScrRez;
-        private ScrRez _scrRez;
-        private PgmMgr _pgmMgr;
 
         public void Bind(Settings settings) {
             _settings = settings;
         }
 
         public void Init() {
-            _scrRez = _settings.ScrRez;
-            // ScrRez(_scrRezArr[3]);
+            SetRez(_settings.ScrRez, 2);
             PgmMgr.Ins.Init();
             PgmMgr.Ins.ExePgm(EPgm.Init);
-
-            // _scrRez.DecideAR();
-            // Debug.Log("AR --> " + _scrRez.AR);
-            Debug.Log("AR --> " + _scrRez.AR+ ", Wd --> " + _scrRez.Wd + ", Ht --> " + _scrRez.Ht);
-            // Debug.Log("AR --> " + (float)Screen.height / (float)Screen.width + ", Wd --> " + Screen.width + ", Ht --> " + Screen.height);
         }
 
         public void InvokeUpd() {
             PgmMgr.Ins.InvokeUpd();
         }
 
-        public void ScrRez(ScrRez scrRez) {
-            // Screen.SetResolution(scrRez.Wd, scrRez.Ht, true);
-            // _currScrRez = scrRez;
+        public void SetRez(ScrRez scrRez) {
+            Screen.SetResolution(scrRez.Wd, scrRez.Ht, true);
+            Debug.Log("Screen Resolution --> Width: " + scrRez.Wd + ", Height: " + scrRez.Ht + ", aspect ratio: " + scrRez.AR);
         }
 
-        public void ScrRez(int wd, int ht) {
-            Screen.SetResolution(wd, ht, true);
+        public void SetRez(ScrRez scrRez, byte lv) {
+            scrRez.Lv = lv;
+            Screen.SetResolution(scrRez.Wd, scrRez.Ht, true);
+            Debug.Log("Screen Resolution --> Width: " + scrRez.Wd + ", Height: " + scrRez.Ht + ", aspect ratio: " + scrRez.AR);
         }
     }
 }
--- Assets/Scripts/ScriptableObject/ScrRez.cs
+++ Assets/Scripts/ScriptableObject/ScrRez.cs
@@ -6,10 +6,10 @@ namespace T {
     [CreateAssetMenu(fileName = "ScrRez", menuName = "ScriptableObject/ScrRez", order = 0)]
     public class ScrRez : ScriptableObject {
 
-        // public DispAR[] DispARArr;
-        public float Max;
-        public float Min;
-        public int NumOfLv;
+        public float Max = 0.0f;
+        public float Min = 0.0f;
+        public byte NumOfLv = 0;
+        public byte Lv = 0;
 
         public bool IsPort { // is portrait or landscape
             get { return Screen.height > Screen.width; }
@@ -21,52 +21,12 @@ namespace T {
 
         public int Wd { // width
             get {
-                if (IsPort) {
-
-                } else {
-
-                }
-                // return IsPort ? Short。 : Long;
-                return 0;
+                return (int)Math.Round((Screen.width * Max - Screen.width * Min) / NumOfLv * Lv + Screen.width * Min);
             }
         }
 
         public int Ht { // height
-            get { return (int)Math.Round(Wd * AR); }
-        }
-
-        // private int[] _longArr;
-        // private int[] _shortArr;
-        // private DispAR _dispAR;
-
-        // private int[] GenrLenArr(int num, int len) { // calculate and return the array of length
-        //     int[] lenArr = new int[num];
-        //     int unit = (int)Math.Round((float)len / (float)num);
-        //     for (int i = 0; i < num; i++) {
-        //         lenArr[i] = unit * (i + 1);
-        //     }
-        //     return lenArr;
-        // }
-
-        // public void DecideAR() {
-        //     DispAR dispAR = DispARArr[0];
-        //     float diff_A = Math.Abs(Ratio - dispAR.ToFloat(IsPort));
-        //     for (byte i = 1; i < DispARArr.Length; i++) {
-        //         float diff_B = Math.Abs(Ratio - DispARArr[i].ToFloat(IsPort));
-        //         if (diff_B < diff_A) {
-        //             diff_A = diff_B;
-        //             dispAR = DispARArr[i];
-        //         }
-        //     }
-        //     _dispAR = dispAR;
-        //     Debug.Log(_dispAR.Long + ":" + _dispAR.Short);
-
-        //     _longArr = GenrLenArr(NumOfLv, IsPort ? Screen.height : Screen.width);
-        //     _shortArr = GenrLenArr(NumOfLv, IsPort ? Screen.width : Screen.height);
-
-        //     for (int i = 0; i < NumOfLv; i++) {
-        //         Debug.Log(_longArr[i] + ":" + _shortArr[i] + " / " + (float)Screen.width + ":" + Screen.height);
-        //     }
+            get { return (int)Math.Round((float)Wd * AR); }
         }
     }
 }
